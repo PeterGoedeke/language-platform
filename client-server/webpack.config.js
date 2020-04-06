551142e60df65b5bc74a69454fd4e2253844d149
PeterGoedeke <peterbgoedeke@gmail.com>
2020-04-06 17:46:33 +1200

    client-server initial commit
    New Vue.js application with support for Vuex, Vue router, SASS, and Pug.

---
+++ client-server/webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-plain-loader'
+            },
+            {
+                test: /\.sass$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            indentedSyntax: true,
+                            sassOptions: {
+                                indentedSyntax: true
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
